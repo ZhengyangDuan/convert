4cded7c3d7d72e62d7e6f4342d2fc1f017a47a69
Zhengyang Duan <zyduan522@gmail.com>
2018-07-19 09:37:13 -0400

    change index view cell

---
+++ ConvertIt/cell/IndexCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCell.swift
+//  ConvertIt
+//
+//  Created by Zhengyang Duan on 2018-07-12.
+//  Copyright © 2018 Zhengyang Duan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IndexCell: UITableViewCell {
+    
+    @IBOutlet weak var btn1: UIButton!
+    
+    @IBOutlet weak var btn2: UIButton!
+    
+    @IBOutlet weak var btn3: UIButton!
+}
