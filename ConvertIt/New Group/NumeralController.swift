b0f8e02050014c2584f4ed70a2f56537b37e8008
Zhengyang Duan <zyduan522@gmail.com>
2018-07-20 11:11:19 -0400

    numeral function conplete

--- ConvertIt/New Group/NumeralController.swift
+++ ConvertIt/New Group/NumeralController.swift
@@ -15,10 +15,12 @@ class NumeralController: UIViewController, UITextFieldDelegate{
     @IBOutlet weak var numeral: UILabel!
     
     @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var numeralSwitch: UISwitch!
     
     @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        numeralSwitch.isOn = false
         
     }
     // MARK: - text field delegate method
@@ -34,11 +36,18 @@ class NumeralController: UIViewController, UITextFieldDelegate{
     }
     // MARK: - UIaction method
     @IBAction func switchActivated(_ sender: UISwitch) {
+        if numeral.text == "Roman to Arabic" {
+            numeral.text = "Arabic to Roman"
+        }else if numeral.text == "Arabic to Roman" {
+            numeral.text = "Roman to Arabic"
+        }
     }
     
     @IBAction func convertPressed(_ sender: UIButton) {
         if numeral.text == "Roman to Arabic" {
             result.text = "\(romanToInt("\(inputField.text!)"))"
+        }else if numeral.text == "Arabic to Roman"{
+            result.text = "\(intToRoman(Int(inputField.text!)!))"
         }
         
         
@@ -62,5 +71,15 @@ class NumeralController: UIViewController, UITextFieldDelegate{
         }
         return res
     }
+    
+    func intToRoman(_ number: Int) -> String {
+        var M = ["","M","MM","MMM"]
+        var C = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+        var X = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+        var I = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+        let s:String = M[number/1000] + C[(number%1000)/100] + X[(number%100)/10] + I[number%10]
+        return s
+    }
+    
     
 }
