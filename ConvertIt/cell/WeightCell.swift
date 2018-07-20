29df00e19e930c550a96d46e492917c782916c16
Zhengyang Duan <zyduan522@gmail.com>
2018-07-20 08:10:49 -0400

    little change on weight page

---
+++ ConvertIt/cell/WeightCell.swift
@@ -0,0 +1,22 @@
+//
+//  WeightCell.swift
+//  ConvertIt
+//
+//  Created by Zhengyang Duan on 2018-07-20.
+//  Copyright © 2018 Zhengyang Duan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class WeightCell1: UITableViewCell
+{
+    
+    
+}
+
+class WeightCell2: UITableViewCell {
+    
+}
+
